1d992389f5e25849dd391d23fb23e0ee32f52efd
Jacek Fedorynski <jfedor@jfedor.org>
2025-04-10 20:42:41 +0200

    Add flatbox_rev9 board

---
+++ boards/flatbox_rev9/board_config.h
@@ -0,0 +1,15 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS 1
+
+#define VENDOR_NAME "Hosaka"
+#define PRODUCT_NAME "Flatbox rev9"
+#define VOLUME_LABEL "FLATBOXREV9"
+#define INDEX_URL "https://github.com/jfedor2/flatbox"
+#define BOARD_ID "SAMD21E18A-Flatbox-rev9"
+
+#define USB_VID 0xCAFE
+#define USB_PID 0xBAF9
+
+#endif
