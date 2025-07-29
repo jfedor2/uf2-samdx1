dbd31a9175e6971fd28b92a7c36066b15139c652
Jacek Fedorynski <jfedor@jfedor.org>
2025-07-29 12:56:12 +0200

    Add pixelrings board

---
+++ boards/pixelrings/board_config.h
@@ -0,0 +1,18 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS 1
+
+#define VENDOR_NAME "Arasaka"
+#define PRODUCT_NAME "Pixelrings"
+#define VOLUME_LABEL "PIXELRINGS"
+#define INDEX_URL "https://github.com/jfedor2/pixelrings"
+#define BOARD_ID "SAMD21E18A-Pixelrings"
+
+#define USB_VID 0xCAFE
+#define USB_PID 0xBAFB
+
+#define BOARD_NEOPIXEL_PIN PIN_PA15
+#define BOARD_NEOPIXEL_COUNT 12
+
+#endif
